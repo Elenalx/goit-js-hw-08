ad8e45ef62420bfba9f4390dd795f75eb6ddb146
Elena Blagodir <elenalx28@gmail.com>
2022-12-13 13:57:51 +0200

    dz 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const createGallery = makeCreateGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallery);
+
+function makeCreateGallery(items) {
+  return items
+    .map(({ preview, description, original }) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+}
+
+const gallery = new SimpleLightbox('.gallery a', { 
+
+    captionsData: 'alt',
+    captionsDelay: 250,
+    overlayOpacity: 0.8,
+    scrollZoom: false,
+});
 
 console.log(galleryItems);
+ 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const key = 'feedback-form-state';
+
+
+form.elements[0].setAttribute('required', '');
+form.elements[1].setAttribute('required', '');
+
+const storageData = localStorage.getItem(key);
+if (storageData) {
+  let storageDataObj = JSON.parse(storageData);
+
+  form.email.value = storageDataObj.email;
+  form.message.value = storageDataObj.message;
+}
+
+const storageDataEl = () => {
+  localStorage.setItem(
+    key,
+    JSON.stringify({
+      email: form.email.value,
+      message: form.message.value,
+    })
+  );
+};
+form.addEventListener('input', throttle(storageDataEl, 500));
+
+const onSubmit = ev => {
+  ev.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(key)));
+  localStorage.removeItem(key);
+  form.reset();
+};
+form.addEventListener('submit', onSubmit);
 
